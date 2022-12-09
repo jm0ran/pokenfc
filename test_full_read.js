2d82653f6f2d2956c78f9c3e56746fd3ab87466e
Joseph Moran <josephdeargmoran@protonmail.com>
2022-12-08 20:55:23 -0500

    Reading of a range of sectors on the card, working on writing now

--- test_full_read.js
+++ test_full_read.js
@@ -9,6 +9,81 @@ const acceptedCardType = "TAG_ISO_14443_3"
 const test_block_num = 4 //Try to stay out of first sector for testing, don't want to screw up the first block
 const key_type = KEY_TYPE_B
 const key = "FFFFFFFFFFFF" //This is Key B, pretty sure it will work as K A tho too
+const lower_sector = 3
+const upper_sector = 4
+
+async function read_blocks(reader){
+    try{
+        total_data = Buffer.alloc(16 * (upper_sector - lower_sector + 1) * 3)
+        buffer_index = 0
+        //Going to have to create a variable to hold data up here
+        for(let i = lower_sector; i <= upper_sector; i++){
+            await reader.authenticate((i - 1) * 4, key_type, key)
+            for(let j = (i - 1) * 4; j < i * 4 - 1; j++){ //This gives us only valid block numbers in a mifare classic 1k card
+                data = await reader.read(j, 16, 16)
+                //Iterate through each piece of data and add it to the larger object
+                for(let m = 0; m < data.length; m ++){
+                    total_data[buffer_index] = data[m]
+                    buffer_index += 1
+                }
+            }
+        }
+        console.log(total_data.length)
+        //Convert and return the data down here
+        // console.log(total_data)
+        test_data = Buffer.from("Hello World", "utf-8")
+        // console.log(test_data)
+    }catch(err){
+        console.log(err)
+    }
+}
+
+async function write_blocks(reader, string){
+    try{
+        data = Buffer.from(string, "utf-8")
+        if(data.length > 16 * (upper_sector - lower_sector + 1) * 3){
+            throw "Error: Data is too long"
+        }
+        console.log("Data is of the right size!")
+        // buffer_index = 0
+        // //Going to have to create a variable to hold data up here
+        // for(let i = lower_sector; i <= upper_sector; i++){
+        //     await reader.authenticate((i - 1) * 4, key_type, key)
+        //     for(let j = (i - 1) * 4; j < i * 4 - 1; j++){ //This gives us only valid block numbers in a mifare classic 1k card
+        //         data = await reader.read(j, 16, 16)
+        //         //Iterate through each piece of data and add it to the larger object
+        //         for(let m = 0; m < data.length; m ++){
+        //             total_data[buffer_index] = data[m]
+        //             buffer_index += 1
+        //         }
+        //     }
+        // }
+        // console.log(total_data.length)
+        // //Convert and return the data down here
+        // // console.log(total_data)
+        // test_data = Buffer.from("Hello World", "utf-8")
+        // // console.log(test_data)
+    }catch(err){
+        console.log(err)
+    }
+}
+
+// async function read_blocks(blocks, reader){
+//     total_data = Buffer.alloc(0)
+//     //Going to have to create a variable to hold data up here
+//     for(let i = lower_sector; i <= upper_sector; i++){
+//         await reader.authenticate((i - 1) * 4, key_type, key)
+//         for(let j = (i - 1) * 4; j < i * 4 - 1; j++){ //This gives us only valid block numbers in a mifare classic 1k card
+//             data = await reader.read(j, 16, 16)
+//             total_data = Buffer.concat([total_data, data])
+//         }
+//     }
+//     //Convert and return the data down here
+//     console.log(total_data)
+//     test_data = Buffer.from("Hello World", "utf-8")
+//     console.log(test_data)
+// }
+
 
 nfc.on('reader', async reader => {
     console.log(`${reader.reader.name} is ready!`);
@@ -18,18 +93,10 @@ nfc.on('reader', async reader => {
             console.log(`Good Card: ${card.type}`);
         }else{
             console.log(`Reader does not accept cards of type: ${card.type}`);
+            return
         }
-
-        //This is where we are going to try and authenticate
-        try{
-            await reader.authenticate(test_block_num, key_type, key)
-            console.log("Successfully Authenticated")
-        }
-        catch(err){
-            console.log(err)
-        }
-
-        
+        // read_blocks(reader)
+        write_blocks(reader, "Hello World!")
     })
 
     reader.on("error", err => {
